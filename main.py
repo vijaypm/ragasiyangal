2a942c1cc6101bf5037094a25b88f5888e65c679
VIJAY MARIADASSOU <vijay@VIJAYs-MacBook-Pro.local>
2020-07-03 17:36:48 -0700

    converted from treeview to grid

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from tkinter import *
+import tkinter as tk
 import tkinter.ttk as ttk
 import csv
 
 def createHeader():
-    label = Label(
+    label = tk.Label(
         text="Hello, Tkinter",
         fg="black",  # Set the text color to white
         bg="green yellow"  # Set the background color to black
@@ -11,45 +11,51 @@ def createHeader():
     label.pack()
 
 def createTable(window):
-    TableMargin = Frame(window, width=500)
-    TableMargin.pack(side=TOP)
-    scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
-    scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
-    tree = ttk.Treeview(TableMargin, columns=("Account", "Username", "Password", "Comments"),
-                        height=400, selectmode="extended",
-                        yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
-    tree.tag_configure('odd', background='#E0FFFF')
-    tree.tag_configure('even', background='#E6E6FA')
-    scrollbary.config(command=tree.yview)
-    scrollbary.pack(side=RIGHT, fill=Y)
-    scrollbarx.config(command=tree.xview)
-    scrollbarx.pack(side=BOTTOM, fill=X)
-    tree.heading('Account', text="Account", anchor=W)
-    tree.heading('Username', text="Username", anchor=W)
-    tree.heading('Password', text="Password", anchor=W)
-    tree.heading('Comments', text="Comments", anchor=W)
-    tree.column('#0', stretch=NO, minwidth=0, width=0)
-    tree.column('#1', stretch=NO, minwidth=0, width=200)
-    tree.column('#2', stretch=NO, minwidth=0, width=200)
-    tree.column('#3', stretch=NO, minwidth=0, width=300)
-    tree.pack()
-    return tree
+    table = tk.LabelFrame(window, text="Passwords")
+    table.grid_columnconfigure(0, weight=1)
+    tk.Label(table, text="Account", anchor="w").grid(row=0, column=0, sticky="ew")
+    tk.Label(table, text="User", anchor="w").grid(row=0, column=1, sticky="ew")
+    tk.Label(table, text="Password", anchor="w").grid(row=0, column=2, sticky="ew")
+    tk.Label(table, text="Comments", anchor="w").grid(row=0, column=3, sticky="ew")
+    tk.Label(table, text="Update", anchor="w").grid(row=0, column=4, sticky="ew")
+    tk.Label(table, text="Delete", anchor="w").grid(row=0, column=5, sticky="ew")
+    table.pack(side="top", fill="both", expand=True, padx=10, pady=10)
+    return table
 
-def populateTable(tree):
+def populateTable(table):
     with open('ids.csv') as f:
         reader = csv.DictReader(f, delimiter=',')
         odd = True
+        idx = 1
         for row in reader:
-            account = row['Account']
-            username = row['Username']
-            password = row['Password']
-            comments = row['Comments']
-            tree.insert("", 0, values=(account, username, password, comments), tags=(('even', 'odd')[odd],))
-            odd = not odd
+            bgcolor = ('#E0FFFF', '#E6E6FA')[odd]
+            account = tk.Entry(table)
+            account.insert(0, row['Account'])
+            account.grid(row=idx, column=0, sticky="ew")
+            account.configure({"background":bgcolor})
 
+            username = tk.Entry(table)
+            username.insert(0, row['Username'])
+            username.grid(row=idx, column=1, sticky="ew")
+            username.configure({"background":bgcolor})
+
+            password = tk.Entry(table)
+            password.insert(0, row['Password'])
+            password.grid(row=idx, column=2, sticky="ew")
+            password.configure({"background":bgcolor})
+
+            comments = tk.Entry(table)
+            comments.insert(0, row['Comments'])
+            comments.grid(row=idx, column=3, sticky="ew")
+            comments.configure({"background":bgcolor})
+
+            tk.Checkbutton(table, onvalue=True, offvalue=False).grid(row=idx, column=4, sticky="ew")
+            tk.Checkbutton(table, onvalue=True, offvalue=False).grid(row=idx, column=5, sticky="ew")
+            odd = not odd
+            idx += 1
 
 def setupWindow():
-    window = Tk()
+    window = tk.Tk()
     window.title("Password Manager")
     width = 1000
     height = 400
