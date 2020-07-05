07ed2829fad3e93d2975507a1567f87a0766ad62
VIJAY MARIADASSOU <vijay@VIJAYs-MacBook-Pro.local>
2020-07-05 09:08:13 -0700

    added an add button and refactored to allow adding row to table

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 import csv
 
-sv_list = []
-bv_list = []
-
+#TODO convert the individual column lists into a single list of a data object
+ac_sv_list = []
+user_sv_list = []
+pass_sv_list = []
+comments_sv_list = []
+updt_bv_list = []
+del_bv_list = []
+numrows = 0
 
 def createHeader():
     label = tk.Label(
@@ -27,57 +32,66 @@ def createTable(window):
 def populateTable(table):
     with open('ids.csv') as f:
         reader = csv.DictReader(f, delimiter=',')
-        odd = True
-        idx = 1
         for row in reader:
-            bgcolor = ('#E0FFFF', '#E6E6FA')[odd]
-            sv = tk.StringVar()
-            bv = tk.BooleanVar()
-            sv_list.append(sv)
-            bv_list.append(bv)
-
-            account = tk.Entry(table, textvariable=sv)
-            account.insert(0, row['Account'])
-            account.grid(row=idx, column=0, sticky="ew")
-            account.configure({"background":bgcolor})
-            sv.trace("w", lambda name, index, mode, var=bv: callback(var))
-            # account.pack()
-
-            username = tk.Entry(table)
-            username.insert(0, row['Username'])
-            username.grid(row=idx, column=1, sticky="ew")
-            username.configure({"background":bgcolor})
-            # username.pack()
-
-            password = tk.Entry(table)
-            password.insert(0, row['Password'])
-            password.grid(row=idx, column=2, sticky="ew")
-            password.configure({"background":bgcolor})
-            # password.pack()
-
-            comments = tk.Entry(table)
-            comments.insert(0, row['Comments'])
-            comments.grid(row=idx, column=3, sticky="ew")
-            comments.configure({"background":bgcolor})
-            # comments.pack()
-
-            up_btn = tk.Checkbutton(table, variable=bv, onvalue=True, offvalue=False)
-            up_btn.grid(row=idx, column=4, sticky="ew")
-            # up_btn.pack()
-
-            del_btn = tk.Checkbutton(table, onvalue=True, offvalue=False)
-            del_btn.grid(row=idx, column=5, sticky="ew")
-            # del_btn.pack()
-
-            odd = not odd
-            idx += 1
-
-# def callbackUpdateCheck(event):
-#     print(event)
+            addRow(table, row)
 
 def callback(bool_var):
     print(bool_var)
     bool_var.set(True)
+
+def addRow(table, row):
+    global numrows
+    numrows += 1
+    bgcolor = ('#E0FFFF', '#E6E6FA')[numrows % 2]
+    ac_sv = tk.StringVar()
+    ac_sv_list.append(ac_sv)
+    user_sv = tk.StringVar()
+    user_sv_list.append(user_sv)
+    pass_sv = tk.StringVar()
+    pass_sv_list.append(pass_sv)
+    comments_sv = tk.StringVar()
+    comments_sv_list.append(comments_sv)
+    updt_bv = tk.BooleanVar()
+    updt_bv_list.append(updt_bv)
+    del_bv = tk.BooleanVar()
+    del_bv_list.append(del_bv)
+
+    account = tk.Entry(table, textvariable=ac_sv)
+    account.insert(0, row['Account'])
+    account.grid(row=numrows, column=0, sticky="ew")
+    account.configure({"background": bgcolor})
+    ac_sv.trace("w", lambda name, index, mode, var=updt_bv: callback(var))
+
+    username = tk.Entry(table, textvariable=user_sv)
+    username.insert(0, row['Username'])
+    username.grid(row=numrows, column=1, sticky="ew")
+    username.configure({"background": bgcolor})
+    user_sv.trace("w", lambda name, index, mode, var=updt_bv: callback(var))
+
+    password = tk.Entry(table, textvariable=pass_sv)
+    password.insert(0, row['Password'])
+    password.grid(row=numrows, column=2, sticky="ew")
+    password.configure({"background": bgcolor})
+    pass_sv.trace("w", lambda name, index, mode, var=updt_bv: callback(var))
+
+    comments = tk.Entry(table, textvariable=comments_sv)
+    comments.insert(0, row['Comments'])
+    comments.grid(row=numrows, column=3, sticky="ew")
+    comments.configure({"background": bgcolor})
+    comments_sv.trace("w", lambda name, index, mode, var=updt_bv: callback(var))
+
+    up_btn = tk.Checkbutton(table, variable=updt_bv, onvalue=True, offvalue=False)
+    up_btn.grid(row=numrows, column=4, sticky="ew")
+
+    del_btn = tk.Checkbutton(table, variable=del_bv, onvalue=True, offvalue=False)
+    del_btn.grid(row=numrows, column=5, sticky="ew")
+    return
+
+def addBtnClicked(table):
+    print("ADD button clicked")
+    row = {'Account':'', 'Username':'', 'Password':'', 'Comments':''}
+    addRow(table, row)
+
 
 def setupWindow():
     window = tk.Tk()
@@ -99,4 +113,6 @@ if __name__ == '__main__':
     table = createTable(root)
     populateTable(table)
     table.pack(side="top", fill="both", expand=True, padx=10, pady=10)
+    add_btn = add_btn = tk.Button(root, text='ADD', command=lambda: addBtnClicked(table))
+    add_btn.pack(side=tk.LEFT)
     root.mainloop()
