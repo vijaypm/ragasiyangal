15c52098fb2109932e1b81aad25983935d82ec07
Vijay Mariadassou <vijaypm.dev@gmail.com>
2024-12-09 22:28:22 -0800

    version bump after migrating to pyqt6

--- ragasiyangal.py
+++ ragasiyangal.py
@@ -28,7 +28,7 @@ from cryptography.hazmat.primitives.ciphers import (
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-_VERSION_ = "v1.0.4"
+_VERSION_ = "v1.0.5"
 
 class CSVTableModel(QAbstractTableModel):
 
